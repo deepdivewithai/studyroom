bfd4c3e9665aa8100461faef88a7c7eca2f39959
hemant jadhav <webdev.hemant@gmail.com>
2023-09-29 13:06:55 +0530

    first commit

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from base.views import home, room
+from base import views
 
 app_name = 'base'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('room/<str:pk>', room, name='room')
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
 ]
