c9b0abdaec1a61f84262ebdbc985d6dfed6cb6b9
HEMANT <webdev.hemant@gmail.com>
2023-09-30 13:18:00 +0530

    first commit

--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
-# rooms = [
-#     {'id': 1 , 'name': 'Lets Learn Python'},
-#     {'id': 2 , 'name': 'Design With Me'},
-#     {'id': 3 , 'name': 'Front End Developers'},
-# ]
 rooms = Room.objects.all()
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains = q)
-    
+    rooms = Room.objects.filter(Q(topic__name__icontains = q) |
+                                Q(name__icontains = q) |
+                                Q(description__icontains = q) )
+    room_count = rooms.count()
     topics = Topic.objects.all()
     context = {'rooms': rooms,
-               'topics': topics}
+               'topics': topics,
+               'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
